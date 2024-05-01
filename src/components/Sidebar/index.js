4ae945cabf73c107cd3614dcb864376873d89113
Swapnil Sinha <swapnilsinha@swapnils-mbp-2.mynetworksettings.com>
2024-04-30 21:45:42 -0400

    Add linkedIn and Github button on sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -16,13 +17,43 @@ const Sidebar = () => (
       <NavLink exact="true" activeclassName="active" to="/home">
         <FontAwesomeIcon icon={faHome} color="#ffffff"></FontAwesomeIcon>
       </NavLink>
-      <NavLink exact="true" activeclassName="active" className="about-link" to="/about">
+      <NavLink
+        exact="true"
+        activeclassName="active"
+        className="about-link"
+        to="/about"
+      >
         <FontAwesomeIcon icon={faUser} color="#ffffff"></FontAwesomeIcon>
       </NavLink>
-      <NavLink exact="true" activeclassName="active" className="contact-link" to="/contact">
+      <NavLink
+        exact="true"
+        activeclassName="active"
+        className="contact-link"
+        to="/contact"
+      >
         <FontAwesomeIcon icon={faEnvelope} color="#ffffff"></FontAwesomeIcon>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://www.linkedin.com/in/swapnilaryansinha/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#ffffff" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://github.com/SwapnilAryan97"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#ffffff" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
