4d11c1e1d4899e1884e962263c25e5a06e2c5ce3
Swapnil Sinha <swapnilsinha@swapnils-mbp-2.mynetworksettings.com>
2024-05-03 12:10:58 -0400

    Fix first test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const LAYOUT_COMPONENT = 'Layout component'
+const HOME_COMPONENT = 'Home component'
+const { container } = render(
+  <MemoryRouter initialEntries={['/']}>
+    <App />
+  </MemoryRouter>
+)
+
+// Mock components used in the routes
+jest.mock('./components/Layout', () => () => LAYOUT_COMPONENT)
+
+jest.mock('./components/Home', () => () => HOME_COMPONENT)
+
+describe('App component', () => {
+  it('renders Layout component for the "/" route', () => {
+    expect(container.innerHTML).toContain(LAYOUT_COMPONENT)
+  })
+
+  // it('renders Home component for the "/" route', () => {
+  //   const { container } = render(
+  //     <MemoryRouter>
+  //       <App />
+  //     </MemoryRouter>
+  //   )
+  //   expect(container.innerHTML).toContain(HOME_COMPONENT)
+  // })
+})
