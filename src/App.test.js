ce177a81aad636c0148c817c684ba805ef2e2707
Swapnil Sinha <swapnilsinha@Swapnils-MBP-2.lan>
2024-05-06 15:00:25 -0400

    temporarily mute  test

--- src/App.test.js
+++ src/App.test.js
@@ -5,28 +5,36 @@ import App from './App'
 
 const LAYOUT_COMPONENT = 'Layout component'
 const HOME_COMPONENT = 'Home component'
-const { container } = render(
-  <MemoryRouter initialEntries={['/']}>
-    <App />
-  </MemoryRouter>
-)
 
 // Mock components used in the routes
 jest.mock('./components/Layout', () => () => LAYOUT_COMPONENT)
 
-jest.mock('./components/Home', () => () => HOME_COMPONENT)
 
 describe('App component', () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  )
+
   it('renders Layout component for the "/" route', () => {
     expect(container.innerHTML).toContain(LAYOUT_COMPONENT)
   })
-
-  // it('renders Home component for the "/" route', () => {
-  //   const { container } = render(
-  //     <MemoryRouter>
-  //       <App />
-  //     </MemoryRouter>
-  //   )
-  //   expect(container.innerHTML).toContain(HOME_COMPONENT)
-  // })
 })
+
+// jest.mock('./components/Home', () => () => HOME_COMPONENT)
+// describe('App component', () => {
+//   const { container } = render(
+//     <MemoryRouter initialEntries={['/']}>
+//       <App />
+//     </MemoryRouter>
+//   )
+//     it('renders Home component for the "/" route', () => {
+//       const { container } = render(
+//         <MemoryRouter initialEntries={['/']}>
+//           <App />
+//         </MemoryRouter>
+//       )
+//       expect(container.innerHTML).toContain(HOME_COMPONENT)
+//     })
+// })
