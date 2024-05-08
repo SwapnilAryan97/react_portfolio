f70202788ec072b3d365d5fc8d94bfef06cec925
Swapnil Sinha <saiamthere@gmail.com>
2024-05-08 14:24:40 -0400

    Add about page styling + minor changes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,20 +1,20 @@
 import { Outlet } from 'react-router-dom'
 // import Sidebar from '../Sidebar'
-import Navbar from '../Navbar'
+// import Navbar from '../Navbar'
 import './index.scss'
 // import Home from '../Home'
 
 const Layout = () => {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Sidebar /> */}
       <div className="page">
-        <span className="tags top-tags">Body</span>
+        <span className="tags top-tags">{/* Body */}</span>
         <Outlet />
         {/* <Home/> */}
-        <span className="tags bottom-tags">&lt;/Body&gt;</span>
+        <span className="tags bottom-tags">{/* &lt;/Body&gt; */}</span>
         <br />
-        <span className="tags bottom-tag-html">&lt;/HTML&gt;</span>
+        <span className="tags bottom-tag-html">{/* &lt;/HTML&gt; */}</span>
       </div>
     </div>
   )
